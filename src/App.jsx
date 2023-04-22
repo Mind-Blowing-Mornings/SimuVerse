8e0d94e05cae300bfbe4d57798390880082cc1df
Diwanshu <saidishu1880@gmail.com>
2023-04-22 23:27:58 +0530

    Initial Push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import Section from './components/Section'
+
+function App() {
+
+  return (
+    <Section></Section>
+  )
+}
+
+export default App
---
+++ src/components/Section.jsx
@@ -0,0 +1,47 @@
+import "./Section.css";
+
+export default function Section() {
+  return (
+    <div className="container">
+      <div className="features_div">
+        <div className="box_one grid_item">
+          <div className="heading_div">
+            <img src="../assets/images/3dmodel.svg" className="svg" />
+            <h5>VIRTUAL TRY ON</h5>
+          </div>
+          <span>
+            Allow your customers to try-on any of your products, anywhere,
+            anytime - without the wait of a change room
+          </span>
+        </div>
+        <div className="box_two grid_item">
+          <div className="heading_div">
+            <img src="../assets/images/exchange.svg"  className="svg"/>
+            <h5>REDUCE RETURN</h5>
+          </div>
+          <span>
+          Reduce returns by allowing customers to see your products in hi-res on their body type
+          </span>
+        </div>
+        <div className="box_three grid_item">
+          <div className="heading_div">
+            <img src="../assets/images/exchange.svg" className="svg"/>
+            <h5>INCREASE AOV</h5>
+          </div>
+          <span>
+          Average oder value is bound to increase with this easy and competitive shopping experience
+          </span>
+        </div>
+        <div className="box_four grid_item">
+          <div className="heading_div">
+            <img src="../public/trash.svg" className="svg" />
+            <h5>REDUCE WASTE</h5>
+          </div>
+          <span>
+          Help reduce your eco footprint by providing a solution that minimises material wastage or damaged stock
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
