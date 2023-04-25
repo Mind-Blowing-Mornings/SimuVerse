ed3ca415584fd6b54b2e74af61af6a753b9bff36
sonali <sonalidhikale0@gmail.com>
2023-04-25 22:14:06 +0530

    Updated Works.css

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -6,32 +6,41 @@ import link1 from "../../assets/images/link1.png";
 
 function Works() {
   return (
-    <div className="App">
-      <p className="p">How it works</p>
-      <div className="img">
-        <img src={image} alt="images" />
-      </div>
-
-      <div className="img1">
-        <img src={image1} alt="selected cloth" />
-        <p>selected cloth</p>
-      </div>
-
-      <div className="img2">
-        <img src={image2} alt="images" />
-        <p>Your beautiful image</p>
-      </div>
-
-      <div className="link1">
+  <>
+    <div>
+    <p className="p">How it works</p>
+    </div>
+    <div className="Works_wrapper">
+      
+      <div className="Works_container">
+      
+        <div className="Left_side">
+          <div className="cloth" id="shirt">
+          <img src={image1} alt="images" />
+          <p>selected cloth</p>
+          </div>
+          <div className="cloth" id="blue_women">
+          <img src={image2} alt="images" />
+          <p>Your beautiful image</p>
+          </div>
+        </div>
+        <div className="Middle_side">
         <img src={link1} alt="images" />
-      </div>
+        </div>
+        <div className="Right_side">
+        <img src={image} alt="images" />
+        </div>
 
-      <p className="text">
+      </div>
+    </div>
+    <div>
+        <p className="text">
         Select the clothing item you liked and use the avatar that you created
         to find the best fit for you.
-      </p>
-    </div>
+       </p>
+       </div>
+    </>
   );
-}
+};
 
 export default Works;
